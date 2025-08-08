a8b29312069d97d3541fe480f26ec79731c7439b
Isamel Shen <isamelshen@gmail.com>
2025-08-08 18:50:32 -0400

    feat(tasks): add tasksController with endpoint handlers

---
+++ server/src/controllers/tasksController.js
@@ -0,0 +1,27 @@
+function tasksGet(req, res) {
+ // TODO
+}
+
+function tasksPost(req, res) {
+  // TODO
+}
+
+function taskGet(req, res) {
+  // TODO
+}
+
+function taskPatch(req, res) {
+  // TODO
+}
+
+function taskDelete(req, res) {
+  // TODO
+}
+
+export {
+  tasksGet,
+  tasksPost,
+  taskGet,
+  taskPatch,
+  taskDelete
+};
