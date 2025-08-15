4e2e785bb2e6830fd975311ae7c78fdde6bac157
Isamel Shen <isamelshen@gmail.com>
2025-08-14 21:02:21 -0400

    feat(server): add tasks router and global error handling

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import tasksRouter from './routes/tasksRouter.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use('/api/tasks', tasksRouter);
+
 app.get('/', (req, res) => {
   res.send("Welcome to Planic!");
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  res.status(err.status || 500).json({
+    message: err.publicMessage || 'Internal server error',
+    code: err.code || 'INTERNAL_ERROR'
+  });
 });
 
 app.listen(PORT, () => {
