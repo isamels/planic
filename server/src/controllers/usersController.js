25b60ba216fe87913655f4518ce34d87bb79bec4
Isamel Shen <isamelshen@gmail.com>
2025-08-16 23:29:27 -0400

    feat(users): add usersController with endpoint handlers

---
+++ server/src/controllers/usersController.js
@@ -0,0 +1,29 @@
+import * as db from '../models/usersModel.js';
+
+async function signupPost() {
+  // todo
+}
+
+async function loginPost() {
+  // todo
+}
+
+async function logoutPost() {
+  // todo
+}
+
+async function accountGet() {
+  // todo
+}
+
+async function accountDelete() {
+  // todo
+}
+
+export {
+  signupPost,
+  loginPost,
+  logoutPost,
+  accountGet,
+  accountDelete
+}
