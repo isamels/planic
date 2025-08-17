9bfb6cc6d926af8b281db1f2f6505963d4177243
Isamel Shen <isamelshen@gmail.com>
2025-08-16 23:28:09 -0400

    feat(tasks): add JWT authentication to taskRouter routes

--- server/src/routes/tasksRouter.js
+++ server/src/routes/tasksRouter.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
+import authenticate from '../middleware/auth.js';
 import * as tasksController from '../controllers/tasksController.js';
 
 const tasksRouter = Router();
 
-tasksRouter.get('/', asyncHandler(tasksController.tasksGet));
-tasksRouter.post('/', asyncHandler(tasksController.tasksPost));
-tasksRouter.get('/:id', asyncHandler(tasksController.taskGet));
-tasksRouter.put('/:id', asyncHandler(tasksController.taskPut));
-tasksRouter.delete('/:id', asyncHandler(tasksController.taskDelete));
+tasksRouter.get('/', authenticate, asyncHandler(tasksController.tasksGet));
+tasksRouter.post('/', authenticate, asyncHandler(tasksController.tasksPost));
+tasksRouter.get('/:id', authenticate, asyncHandler(tasksController.taskGet));
+tasksRouter.put('/:id', authenticate, asyncHandler(tasksController.taskPut));
+tasksRouter.delete('/:id', authenticate, asyncHandler(tasksController.taskDelete));
 
 export default tasksRouter;
