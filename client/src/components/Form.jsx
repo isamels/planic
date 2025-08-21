c264b739e05c08fd0cb7a118f1392491859a9252
Isamel Shen <isamelshen@gmail.com>
2025-08-21 15:05:33 -0400

    feat(users): add error display to Form component

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,6 +1,9 @@
-export default function Form({ onSubmit }) {
+import ErrorMessage from './ErrorMessage.jsx';
+
+export default function Form({ onSubmit, error }) {
   return (
     <div>
+      <ErrorMessage message={error} />
       <form onSubmit={onSubmit}>
         <label htmlFor="username">Username: </label>
         <input type="text" name="username" id="username" required />
