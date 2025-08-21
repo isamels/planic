6193da5ff9e229224d1162126266278c16047155
Isamel Shen <isamelshen@gmail.com>
2025-08-21 14:28:17 -0400

    feat(users): add axios client for users API

---
+++ client/src/api/user.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_BASE = "http://localhost:3000/users";
+
+const api = axios.create({
+  baseURL: API_BASE
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+const signup = (userData) => api.post(`${API_BASE}/signup`, userData);
+const login = (userData) => api.post(`${API_BASE}/login`, userData);
+const getAccount = () => api.get(`${API_BASE}/account`);
+const deleteAccount = () => api.delete(`${API_BASE}/account`);
+
+export { signup, login, getAccount, deleteAccount };
