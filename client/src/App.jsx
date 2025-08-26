8c8241eb435261d8f937ddb116c3a86afb88bc8e
Isamel Shen <isamelshen@gmail.com>
2025-08-26 12:43:39 -0400

    feat(ui): add react routing to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthProvider from './AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <h1>Planic!</h1>
-      </AuthProvider>
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={
+              <h1>Planic!</h1>
+            } />
+          </Routes>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
