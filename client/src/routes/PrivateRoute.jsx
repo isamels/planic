2aa842e8b681ec9e8109153fe2b506706d54de87
Isamel Shen <isamelshen@gmail.com>
2025-08-26 13:16:07 -0400

    feat(ui): add PrivateRoute component for protected routes

---
+++ client/src/routes/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../AuthProvider';
+
+export default function PrivateRoute() {
+  const user = useAuth();
+  
+  if (!user.token) {
+    return <Navigate to="/login" />;
+  }
+  
+  return <Outlet />;
+}
