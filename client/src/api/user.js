f295f74915bdf63e778318decc0c74c1d86bc674
Isamel Shen <isamelshen@gmail.com>
2025-08-27 00:18:03 -0400

    fix(users): update API base URL

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:3000/users";
+
+const API_BASE = "http://localhost:3000/api/users";
 
 const api = axios.create({
   baseURL: API_BASE
