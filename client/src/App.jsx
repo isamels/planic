30a616ff77052bb72616f5c7fa1d58abf55009be
Isamel Shen <isamelshen@gmail.com>
2025-08-27 00:19:27 -0400

    feat(ui): add public and private routes with dashboard and account pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthProvider from './AuthProvider';
+import PublicRoute from './routes/PublicRoute';
 import PrivateRoute from './routes/PrivateRoute';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Account from './pages/Account';
 
 function App() {
   return (
@@ -10,9 +14,13 @@ function App() {
       <Router>
         <AuthProvider>
           <Routes>
-            <Route path='/login' element={<Login />} />
+            <Route element={<PublicRoute />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Route>
             <Route element={<PrivateRoute />}>
-              <Route path='/' element={<p>Planic!</p>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/account" element={<Account />} />
             </Route>
           </Routes>
         </AuthProvider>
