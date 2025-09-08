2e29f5602b88d5e1f394796646af9af5077fe955
Isamel Shen <isamelshen@gmail.com>
2025-09-08 00:28:24 -0700

    feat(availability): implement default and date hours controllers

--- server/src/controllers/availabilityController.js
+++ server/src/controllers/availabilityController.js
@@ -1,17 +1,25 @@
+import * as db from '../models/availabilityModel.js';
+
 async function defaultHoursGet(req, res) {
-  res.json({ hours: 5 });
+  const { hours } = await db.getDefaultHours(req.user.id, req.query.day);
+  res.json({ hours });
 }
 
 async function defaultHoursPost(req, res) {
-  res.json({ hours: 5 });
+  const { day, hours } = req.body;
+  const availability = await db.setDefaultHours(req.user.id, day, hours);
+  res.status(201).json({ hours: availability.hours });
 }
 
 async function dateHoursGet(req, res) {
-  res.json({ hours: 5 });
+  const { hours } = await db.getDateHours(req.user.id, req.query.date);
+  res.json({ hours });
 }
 
 async function dateHoursPost(req, res) {
-  res.json({ hours: 5 });
+  const { date, hours } = req.body;
+  const availability = await db.setDateHours(req.user.id, date, hours);
+  res.status(201).json({ hours: availability.hours });
 }
 
 export { defaultHoursGet, defaultHoursPost, dateHoursGet, dateHoursPost };
